c27dabb84cace3e8afb89948194d60d54bf786ad
Splavski Maxim <mox7.sp1avsk1@gmail.com>
2026-05-07 01:33:43 +0300

    README small fix

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,9 @@
 <?php
 
 define('APP_NAME', 'UrbanNest Estate');
-define('BASE_URL', '/projects/usm/LI/Splavski%20Maxim%20-%20Metelska%20Daniela/public');
+define('BASE_URL', '/projects/usm/li/Splavski%20Maxim%20-%20Metelska%20Daniela/public');
 
 define('DB_HOST', '127.0.0.1');
 define('DB_NAME', 'urbannest_estate');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-
